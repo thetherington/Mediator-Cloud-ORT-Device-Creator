7e16fb31b9714d563d8aab40eac99014fdba1468
thetherington <thomas@hetheringtons.org>
2021-04-05 12:45:03 -0400

    added logging system for both classes to streaming out and syslog

--- scripts/CloudORTCreator.py
+++ scripts/CloudORTCreator.py
@@ -1,5 +1,9 @@
 import copy
 import json
+import logging
+import logging.handlers
+import socket
+import sys
 import uuid
 from threading import Thread
 from xml.dom import minidom
@@ -12,7 +16,9 @@ requests.packages.urllib3.disable_warnings()
 
 
 class DeviceORTCreator:
-    def __init__(self, **kwargs):
+    def __init__(self, logger=logging.getLogger(), **kwargs):
+
+        self.logger = logger
 
         self.proto = "https"
         self.address = "127.0.0.1"
@@ -46,7 +52,7 @@ class DeviceORTCreator:
             # mediator lookup has been enabled
             if key == "mediator":
 
-                self.mediator_lookup = MediatorServiceCollector(**value)
+                self.mediator_lookup = MediatorServiceCollector(self.logger, **value)
                 self.mediator_lookup.collect()
 
             # simple attributes are set as-is
@@ -73,36 +79,63 @@ class DeviceORTCreator:
                     changes = 0
 
                     # fetch the existing insite devices and reference in the device object
+                    # check that it's a valid dictionary
                     device_data = self.fetch_devices(http_session)
-                    devices = device_data["devices"]
 
-                    # iterate through all ort host-to-name annotations
-                    for host, alias in annotations.items():
+                    if isinstance(device_data, dict):
 
-                        # iterate through each device in the devices list and check if
-                        # annotated alias matches a device alias. if no breaking happens
-                        # then the else block runs to create a new device.
-                        for device in devices:
+                        devices = device_data["devices"]
+                        self.logger.info("Number of devices fetched %s", len(devices))
 
-                            if alias == device["identification"]["alias"]:
+                        # iterate through all ort host-to-name annotations
+                        for host, alias in annotations.items():
 
-                                # if mediator lookup is enabled, then use the tags in the device
-                                # to match a system name in the mediator lookup class
-                                if self.mediator_lookup:
+                            # iterate through each device in the devices list and check if
+                            # annotated alias matches a device alias. if no breaking happens
+                            # then the else block runs to create a new device.
+                            for device in devices:
 
-                                    tags = self.mediator_lookup.return_systems(host)
+                                if alias == device["identification"]["alias"]:
 
-                                    # empty tag list means mediator lookup catalog creation failed
-                                    # or mediator is no longer keeping the ort annotions updated in insite
-                                    # break out - ignore this annotation
-                                    if not tags:
-                                        break
+                                    # if mediator lookup is enabled, then use the tags in the device
+                                    # to match a system name in the mediator lookup class
+                                    if self.mediator_lookup:
 
-                                    # if all the tags from the system name lookup matches with devices, then
-                                    # check if a matchables update is needed. otherwise, the devices iteration continues,
-                                    # and then a new device will be made for the new tag. (multiple systems with same ort names)
-                                    # breaking causes the for else to cancel
-                                    if all(tag in device["grouping"]["tags"] for tag in tags):
+                                        tags = self.mediator_lookup.return_systems(host)
+
+                                        # empty tag list means mediator lookup catalog creation failed
+                                        # or mediator is no longer keeping the ort annotions updated in insite
+                                        # break out - ignore this annotation
+                                        if not tags:
+
+                                            self.logger.warning(
+                                                "Missing ORT from Mediator: %s", host
+                                            )
+                                            break
+
+                                        # if all the tags from the system name lookup matches with devices, then
+                                        # check if a matchables update is needed. otherwise, the devices iteration continues,
+                                        # and then a new device will be made for the new tag. (multiple systems with same ort names)
+                                        # breaking causes the for else to cancel
+                                        if all(tag in device["grouping"]["tags"] for tag in tags):
+
+                                            if host not in device["identification"]["matchables"]:
+
+                                                device["identification"]["matchables"].append(host)
+                                                device["identification"]["control-ips"].append(host)
+
+                                                changes += 1
+
+                                                self.logger.warning(
+                                                    "ORT: %s Updated: %s", alias, host
+                                                )
+
+                                            # everthing matches - no changes
+                                            break
+
+                                    # single system so just update the matchables with there is a new hostname
+                                    # breaking causes the for else to cancel.
+                                    else:
 
                                         if host not in device["identification"]["matchables"]:
 
@@ -111,50 +144,45 @@ class DeviceORTCreator:
 
                                             changes += 1
 
+                                            self.logger.warning("ORT: %s Updated: %s", alias, host)
+
                                         # everthing matches - no changes
                                         break
 
-                                # single system so just update the matchables with there is a new hostname
-                                # breaking causes the for else to cancel.
-                                else:
+                            # device iteration completes (no breaking)
+                            # it's determined here that there should be a new device added to the devices list
+                            else:
 
-                                    if host not in device["identification"]["matchables"]:
+                                new_device = copy.deepcopy(self.device_template)
 
-                                        device["identification"]["matchables"].append(host)
-                                        device["identification"]["control-ips"].append(host)
+                                new_device["identification"]["uid"] = str(uuid.uuid4())
+                                new_device["identification"]["alias"] = alias
+                                new_device["identification"]["matchables"].append(host)
+                                new_device["identification"]["control-ips"].append(host)
 
-                                        changes += 1
+                                if self.mediator_lookup:
+                                    new_device["grouping"]["tags"].extend(
+                                        self.mediator_lookup.return_systems(host)
+                                    )
 
-                                    # everthing matches - no changes
-                                    break
+                                devices.append(new_device)
 
-                        # device iteration completes (no breaking)
-                        # it's determined here that there should be a new device added to the devices list
-                        else:
+                                self.logger.info("New Device added: %s (%s)", alias, host)
 
-                            new_device = copy.deepcopy(self.device_template)
+                                changes += 1
 
-                            new_device["identification"]["uid"] = str(uuid.uuid4())
-                            new_device["identification"]["alias"] = alias
-                            new_device["identification"]["matchables"].append(host)
-                            new_device["identification"]["control-ips"].append(host)
+                        # supress pushing new devices when there hasn't been any changes
+                        # otherwise, post everything back
+                        if changes > 0:
 
-                            if self.mediator_lookup:
-                                new_device["grouping"]["tags"].extend(
-                                    self.mediator_lookup.return_systems(host)
-                                )
+                            self.logger.info("Pushing %s Devices", len(device_data["devices"]))
 
-                            devices.append(new_device)
+                            rtrn_devices = self.push_devices(device_data, http_session)
 
-                            changes += 1
+                            self.logger.info("Returned %s Devices", len(rtrn_devices["devices"]))
+                            self.logger.debug(json.dumps(device_data, indent=1))
 
-                    # supress pushing new devices when there hasn't been any changes
-                    # otherwise, post everything back
-                    if changes > 0:
-                        print(json.dumps(device_data, indent=1))
-                        self.push_devices(device_data, http_session)
-
-                    print(changes)
+                        self.logger.info("Changes Recorded: %s", changes)
 
                 self.logout()
 
@@ -171,13 +199,15 @@ class DeviceORTCreator:
                 return json.loads(resp.text)
 
         except Exception as e:
-            print(e)
+            self.logger.critical(e)
 
         return None
 
     def logon(self, http_session=requests):
 
         try:
+
+            self.logger.debug("Logging in as user: %s, pass: %s", self.username, self.password)
 
             logon_params = {"username": self.username, "password": self.password}
             url = "{}://{}/{}".format(self.proto, self.address, self.logon_route)
@@ -190,7 +220,9 @@ class DeviceORTCreator:
                 return resp.status_code
 
         except Exception as e:
-            print(e)
+            self.logger.critical(e)
+
+        self.logger.warning("Logon failed")
 
         return None
 
@@ -206,8 +238,11 @@ class DeviceORTCreator:
                 return json.loads(resp.text)
 
         except Exception as e:
-            print(e)
+            self.logger.critical(e)
 
+        self.logger.warning(
+            "Failed to collect devices, reason: %s, status code: %s", resp.text, resp.status_code
+        )
         return None
 
     def push_devices(self, device_data, http_session=requests):
@@ -224,8 +259,11 @@ class DeviceORTCreator:
                 return json.loads(resp.text)
 
         except Exception as e:
-            print(e)
+            self.logger.critical(e)
 
+        self.logger.warning(
+            "Failed to push devices, reason: %s, status code: %s", resp.text, resp.status_code
+        )
         return None
 
     def logout(self, http_session=requests):
@@ -239,11 +277,11 @@ class DeviceORTCreator:
             return resp.status_code
 
         except Exception as e:
-            print(e)
+            self.logger.warning(e)
 
 
 class MediatorServiceCollector:
-    def __init__(self, **kwargs):
+    def __init__(self, logger=logging.getLogger(), **kwargs):
 
         # self.hosts is a list of tuples [(IP:System Name)]
         self.hosts = []
@@ -252,6 +290,8 @@ class MediatorServiceCollector:
         self.route = "info/scripts/CGItoXML.exe/servicerequest"
         self.sub = None
         self.collection = {}
+
+        self.logger = logger
 
         self.request_services_params = {
             "uniqueid": 0,
@@ -302,7 +342,7 @@ class MediatorServiceCollector:
                 doc = minidom.parseString(str(resp.text))
 
             except Exception as e:
-                print(e)
+                self.logger.critical(e)
                 return None
 
         system_list.extend(
@@ -312,6 +352,8 @@ class MediatorServiceCollector:
                 if get_element(service, "Name") == "OvertureRT Driver"
             ]
         )
+
+        self.logger.info("System: %s, Number of hosts: %s", host[1], len(system_list))
 
     def collect(self):
 
@@ -326,7 +368,7 @@ class MediatorServiceCollector:
         for y in threads:
             y.join()
 
-        print(self.collection)
+        self.logger.debug(self.collection)
 
 
 def main():
@@ -344,7 +386,29 @@ def main():
         },
     }
 
-    ort_creator = DeviceORTCreator(**params)
+    mode = "stdout"
+
+    if mode == "syslog":
+
+        logger = logging.getLogger(socket.gethostname())
+        handler = logging.handlers.SysLogHandler(address=(params["address"], 514), facility=19)
+        formatter = logging.Formatter(
+            "%(asctime)s %(name)s " + "CloudORTCreator[%(process)d]: %(message)s"
+        )
+
+    else:
+
+        logger = logging.getLogger(__name__)
+        handler = logging.StreamHandler(sys.stdout)
+        formatter = logging.Formatter(
+            "%(asctime)s : %(levelname)s : %(name)s %(funcName)s:%(lineno)d : %(message)s"
+        )
+
+    logger.setLevel(logging.INFO)
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+    ort_creator = DeviceORTCreator(logger, **params)
 
     ort_creator.process()
 
