201bfc2dfdd8ee275361a9d58c5b522c52cb4a77
thetherington <thomas@hetheringtons.org>
2021-04-07 14:18:59 -0400

    added args parse support and device clearing option

--- extra/device_import.py
+++ extra/device_import.py
@@ -1,3 +1,4 @@
+import argparse
 import copy
 import csv
 import json
@@ -9,63 +10,6 @@ import urllib3
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 requests.packages.urllib3.disable_warnings()
-
-INSITE = "172.16.112.14"
-CSV_FILE = "cbs_mediator_import.csv"
-
-DEVICE_URL = "https://{}/api/-/settings/device".format(INSITE)
-LOGON_URL = "https://{}/api/v1/login".format(INSITE)
-LOGOUT_URL = "https://{}/api/v1/logout".format(INSITE)
-ANNOTATION_TYPE_URL = "https://{}/api/-/model/catalog/annotation/general-host-to-devicetype".format(
-    INSITE
-)
-ANNOTATION_SERVICENAME_URL = "https://{}/api/-/model/catalog/annotation/general-host-to-servicename".format(
-    INSITE
-)
-
-device_template = {
-    "identification": {
-        "uid": None,
-        "alias": None,
-        "matchables": [],
-        "restrictable-ips": [""],
-        "control-ips": [],
-    },
-    "grouping": {"tags": []},
-}
-
-device_list = []
-annotation_type = {}
-annotation_servicename = {}
-
-if Path("_files/" + CSV_FILE).exists():
-    csv_path = Path("_files/" + CSV_FILE)
-
-elif Path(CSV_FILE).exists():
-    csv_path = Path(CSV_FILE)
-
-with open(str(csv_path), "r") as f:
-
-    reader = csv.reader(filter(lambda row: row[0] != "#", f))
-
-    for row in reader:
-
-        if len(row) > 0:
-
-            device = copy.deepcopy(device_template)
-
-            device["identification"]["uid"] = str(uuid.uuid4())
-            device["identification"]["alias"] = row[0]
-            device["identification"]["matchables"].extend(row[1].split(";"))
-            device["identification"]["control-ips"].extend(row[1].split(";"))
-
-            device["grouping"]["tags"].extend(row[4].split(";"))
-
-            device_list.append(device)
-
-            for match in device["identification"]["matchables"]:
-                annotation_type.update({match: device["grouping"]["tags"][1]})
-                annotation_servicename.update({match: device["grouping"]["tags"][2]})
 
 
 def post(session, url, data):
@@ -157,6 +101,102 @@ def remove_duplicate_devices(x_list, y_db):
     _ = [y_db["devices"].pop(pop) for pop in y_pops]
 
     return len(x_pops), len(y_pops)
+
+
+parser = argparse.ArgumentParser(description="inSITE device importer tool")
+parser.add_argument(
+    "-H", "--host", metavar="", required=False, default="127.0.0.1", help="IP to inSITE Server",
+)
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument("-F", "--file", metavar="", required=False, help="CSV File to Import")
+group.add_argument(
+    "-D",
+    "--dump",
+    required=False,
+    action="store_true",
+    help="Clear out existing device and annotations",
+)
+
+args = parser.parse_args()
+# args = parser.parse_args(["-H", "172.16.112.14", "-F", "_files/cbs_mediator_import.csv"])
+# args = parser.parse_args(["-H", "172.16.112.14", "-D"])
+
+INSITE = args.host
+CSV_FILE = args.file
+
+DEVICE_URL = "https://{}/api/-/settings/device".format(INSITE)
+LOGON_URL = "https://{}/api/v1/login".format(INSITE)
+LOGOUT_URL = "https://{}/api/v1/logout".format(INSITE)
+ANNOTATION_NAME_URL = "https://{}/api/-/model/catalog/annotation/general-host-to-devicename".format(
+    INSITE
+)
+ANNOTATION_TYPE_URL = "https://{}/api/-/model/catalog/annotation/general-host-to-devicetype".format(
+    INSITE
+)
+ANNOTATION_SERVICENAME_URL = "https://{}/api/-/model/catalog/annotation/general-host-to-servicename".format(
+    INSITE
+)
+
+if args.dump:
+
+    with requests.Session() as http_session:
+
+        print("Cleaning devices and annotations...")
+
+        post(http_session, LOGON_URL, {"username": "admin", "password": "admin"})
+        post(http_session, DEVICE_URL, {})
+        put(http_session, ANNOTATION_NAME_URL, {})
+        put(http_session, ANNOTATION_TYPE_URL, {})
+        put(http_session, ANNOTATION_SERVICENAME_URL, {})
+
+    if not args.file:
+        quit()
+
+
+device_template = {
+    "identification": {
+        "uid": None,
+        "alias": None,
+        "matchables": [],
+        "restrictable-ips": [""],
+        "control-ips": [],
+    },
+    "grouping": {"tags": []},
+}
+
+device_list = []
+annotation_type = {}
+annotation_servicename = {}
+
+if Path(CSV_FILE).exists():
+    csv_path = Path(CSV_FILE)
+
+else:
+    print("Invalid CSV File/Path " + CSV_FILE)
+    quit()
+
+with open(str(csv_path), "r") as f:
+
+    reader = csv.reader(filter(lambda row: row[0] != "#", f))
+
+    for row in reader:
+
+        if len(row) > 0:
+
+            device = copy.deepcopy(device_template)
+
+            device["identification"]["uid"] = str(uuid.uuid4())
+            device["identification"]["alias"] = row[0]
+            device["identification"]["matchables"].extend(row[1].split(";"))
+            device["identification"]["control-ips"].extend(row[1].split(";"))
+
+            device["grouping"]["tags"].extend(row[4].split(";"))
+
+            device_list.append(device)
+
+            for match in device["identification"]["matchables"]:
+                annotation_type.update({match: device["grouping"]["tags"][1]})
+                annotation_servicename.update({match: device["grouping"]["tags"][2]})
 
 
 with requests.Session() as http_session:
